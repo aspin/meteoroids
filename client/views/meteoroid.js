d8c4657486db75d6357ece041f4c3682d8cdc633
Michael Wang <mwang@splunk.com>
2015-08-09 11:43:32 -0700

    Fix weird typo

--- client/views/meteoroid.js
+++ client/views/meteoroid.js
@@ -20,28 +20,7 @@ Template.meteoroid.helpers({
   scoreboard: function() {
     // return Scoreboard.find({}).sort({ score: -1 }).limit(10);
     return Scoreboard.find({}, {sort: { score: -1 }}, { limit: 10 });
-  },table.gridtable {
-	font-family: verdana,arial,sans-serif;
-	font-size:11px;
-	color:#333333;
-	border-width: 1px;
-	border-color: #666666;
-	border-collapse: collapse;
-}
-table.gridtable th {
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #666666;
-	background-color: #dedede;
-}
-table.gridtable td {
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #666666;
-	background-color: #ffffff;
-}
+  },
   weapon: function() {
     return Session.get("weapon");
   }
